a71e93e3a2e209b21210f6026acdbb98a6ac7c6d
HishamKader <hisham319@hotmial.com>
2021-02-28 12:59:16 -0500

    app works great and fixed minor bugs

--- app.js
+++ app.js
@@ -4,169 +4,223 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employees = []
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-var team = [];
+start()
+// Write code to use inquirer to gather information about the development team members,
+function start() {
+    welcome();
+    inquirer.prompt({
+        name: "startOption",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Add a new Employee", "Exit"]
+    })
+
+    .then(function(answer) {
+        console.clear();
+
+        // ADD NEW EMPLOYEE
+        if (answer.startOption === "Add a new Employee") {
+            addEmployee()
+        }
+    })
 
 
-// ==== GENERAL FUNCTIONS ==== //
-
-// make an entry point into our inquirer prompts
-function start(){
-    inquirer.prompt(
-        [
-            {
-                name: "startOptions",
-                type: "list",
-                message: "Make a selection: ",
-                choices: [
-                    "Build Team",
-                    "Exit"
-                ]
-            }
-        ]).then((answer) => {
-            if(answer.startOptions === "Build Team"){
-                buildManager();
-            }
-            else{
-                process.exit(1);
-            }
-        })
 }
+// =============================
+// ADD EMPLOYEE FUNCTION
+// =============================
 
-// Asks if the user wants to add more employees after each added employee.
-function addMore(){
-    inquirer.prompt(
-        [
-        {
-            name: "addMoreOptions",
-            type: "list",
-            message: "Add more team members?: ",
-            choices: [
-                "Yes",
-                "No"
-            ]
-        }
-        ]).then((answer)=> {
-            if(answer.addMoreOptions === "Yes"){
-                selectMembers();
-            }
-            else {
-                // some code here.
-            }
-        })
-}
+function addEmployee(){
+    console.clear();
+    employeeWelcome();
+    inquirer.prompt({
+        name: "employeeType",
+        type: "list",
+        message: "What position will the employee have",
+        choices: ["Engineer", "Intern", "Manager"]
+    })
 
-// Provides a list of employee types the user can add.
-function selectMembers(){
-    inquirer.prompt([
-        {
-            name: "memberOptions",
-            type: "list",
-            message: "Add which type of employee? : ",
-            choices: [
-                "Intern",
-                "Engineer",
-                "Quit & Start Over"
-            ]
-        }
-    ]).then((answer)=> {
-        if(answer.memberOptions === "Intern"){
-            buildIntern();
-        }
-        else if (answer.memberOptions === "Engineer"){
-            buildEngineer();
-        } else {
-            process.exit(1);
-        }
+    .then(function(answer) {
+        const type = answer.employeeType;
+        console.clear();
+            console.log("You selected '" + type +"'")
+            if (type === "Engineer") {
+                inquirer.prompt([
+                    {
+                        name: "name",
+                        type: "input",
+                        message: "what is the employee's name?"
+                    },
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "What is their ID number?"   
+                    },
+                    {
+                        name: "email",
+                        type: "input",
+                        message: "what is the employee's email?"
+                    },
+                    {
+                        name: "github",
+                        type: "input",
+                        message: "what is the employee's github name?  It will display as 'https://github.com/GITHUBNAME'"
+                    },
+                ])
+                .then(function(answer) {
+                    let github = ("https://github.com/" + answer.github)
+                    console.log(answer.name + " " + answer.id + " " + answer.email + " " + github)
+                    let employee = new Engineer(answer.name, answer.id, answer.email, github);
+                    
+                    employees.push(employee)
+                    console.log(employee)
+                    inquirer.prompt({
+                        name: "another",
+                        type: "list",
+                        message: "Would you like to add another employee?",
+                        choices: ["Yes", "No"]
+                    })
+
+                    .then(function(answer) {
+                        if (answer.another === "Yes") {
+                            addEmployee();
+                        } else {
+                            renderHtml();
+                                console.log("File is created successfully")
+                        }
+                    })
+                })
+
+            } else if (type === "Intern") {
+                inquirer.prompt([
+                    {
+                        name: "name",
+                        type: "input",
+                        message: "what is the interns name?"
+                    },
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "What is the intern's ID number?"   
+                    },
+                    {
+                        name: "email",
+                        type: "input",
+                        message: "what is the intern's email?"
+                    },
+                    {
+                        name: "school",
+                        type: "input",
+                        message: "what is the intern's school's name?"
+                    },
+                ])
+                .then(function(answer) {
+                    console.log(answer.name + " " + answer.id + " " + answer.email + " " + answer.school)
+                    let employee = new Intern(answer.name, answer.id, answer.email, answer.school);
+                    
+                    employees.push(employee)
+                    console.log(employee)
+                    inquirer.prompt({
+                        name: "another",
+                        type: "list",
+                        message: "Would you like to add another employee?",
+                        choices: ["Yes", "No"]
+                    })
+
+                    .then(function(answer) {
+                        if (answer.another === "Yes") {
+                            addEmployee();
+                        } else {
+                            renderHtml();
+                            console.log("File is created successfully")
+                        }
+                    })
+                })
+            } else if (type === "Manager") {
+                inquirer.prompt([
+                    {
+                        name: "name",
+                        type: "input",
+                        message: "what is the manager's name?"
+                    },
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "What is the mangager's ID number?"   
+                    },
+                    {
+                        name: "email",
+                        type: "input",
+                        message: "what is the manager's email?"
+                    },
+                    {
+                        name: "officeNumber",
+                        type: "input",
+                        message: "what is the manager's office number?"
+                    },
+                ])
+                .then(function(answer) {
+                    console.log(answer.name + " " + answer.id + " " + answer.email + " " + answer.officeNumber)
+                    let employee = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+                    
+                   employees.push(employee)
+                    console.log(employee)
+                    inquirer.prompt({
+                        name: "another",
+                        type: "list",
+                        message: "Would you like to add another employee?",
+                        choices: ["Yes", "No"]
+                    })
+
+                    .then(function(answer) {
+                        if (answer.another === "Yes") {
+                            addEmployee();
+                        } else {
+                            renderHtml();
+                                console.log("File is created successfully")
+                            }
+                        }
+                    )
+                })
+            }
+            
+            
+
     })
 }
 
-
-
-// ==== EMPLOYEE BUILDER FUNCTIONS ==== //
-
-// build team always starts with a manager as that position is required.
-function buildManager(){
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Manager name: "
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Manager email: "
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Manager phone number: "
-        },
-
-    ]).then((answer) => {
-       const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
-       team.push(manager);
-
-       selectMembers();
-    })
+function welcome(){
+    console.clear();
+    console.log(".----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. ");
+    console.log("| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |");
+    console.log("| | _____  _____ | || |  _________   | || |   _____      | || |     ______   | || |     ____     | || | ____    ____ | || |  _________   | |");
+    console.log("| ||_   _||_   _|| || | |_   ___  |  | || |  |_   _|     | || |   .' ___  |  | || |   .'    `.   | || ||_   \\  /   _|| || | |_   ___  |  | |");
+    console.log("| |  | | /\\ | |  | || |   | |_  \\_|  | || |    | |       | || |  / .'   \\_|  | || |  /  .--.  \\  | || |  |   \\/   |  | || |   | |_  \\_|  | |");
+    console.log("| |  | |/  \\| |  | || |   |  _|  _   | || |    | |   _   | || |  | |         | || |  | |    | |  | || |  | |\\  /| |  | || |   |  _|  _   | |");
+    console.log("| |  |   /\\   |  | || |  _| |___/ |  | || |   _| |__/ |  | || |  \\ `.___.'\\  | || |  \\  `--'  /  | || | _| |_\\/_| |_ | || |  _| |___/ |  | |");
+    console.log("| |  |__/  \\__|  | || | |_________|  | || |  |________|  | || |   `._____.'  | || |   `.____.'   | || ||_____||_____|| || | |_________|  | |");
+    console.log("| |              | || |              | || |              | || |              | || |              | || |              | || |              | |");
+    console.log("| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |");
+    console.log("'----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ");   
 }
 
-function buildIntern(){
-    inquirer.prompt([{
-        type: "input",
-        name: "internName",
-        message: "Intern name: "
-    },
-    {
-        type: "input",
-        name: "internEmail",
-        message: "Intern email: "
-    },{
-        name: "internSchool",
-        type: "text",
-        message: "Enter your school: "
-    }
-    ]).then((answer)=>{
-        const intern = new Intern(answer.internName, answer.internEmail,answer.internSchool);
-        team.push(intern);
-
-        // Ask the user if they want to add more team members.
-        addMore();
-    })
+function employeeWelcome() {
+    console.log("   __    ____   ____     ____  __  __  ____  __    _____  _  _  ____  ____ ");
+    console.log("  /__\\  (  _ \\ (  _ \\   ( ___)(  \\/  )(  _ \\(  )  (  _  )( \\/ )( ___)( ___)");
+    console.log(" /(__)\\  )(_) ) )(_) )   )__)  )    (  )___/ )(__  )(_)(  \\  /  )__)  )__) ");
+    console.log("(__)(__)(____/ (____/   (____)(_/\\/\\_)(__)  (____)(_____) (__) (____)(____)");
 }
 
-function buildEngineer(){
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "engineerName",
-            message: "Engineer name: "
-        },
-        {
-            type: "input",
-            name: "engineerEmail",
-            message: "Engineer email: "
-        },
-        {
-        name: "engineerGithub",
-        type: "text",
-        message: "Enter your github username: "
-        }
-    ]).then((answer)=>{
-        const engineer = new Engineer(answer.engineerName, answer.engineerEmail, answer.engineerGithub);
-        team.push(engineer);
-
-        // Ask the user if they want to add more team members.
-        addMore();
-    })
+function renderHtml() {
+    let html = render(employees);
+    fs.writeFileSync(outputPath, html, function (err) {
+    if (err) throw err;}
+    );
 }
-
-render(team);
-
-start();
